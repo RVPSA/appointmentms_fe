a398baa0ed1032cad66f0d956d5200c14a5474db
RVPSA <akeshramasinghe@gmail.com>
2025-02-24 19:51:01 +0100

    comment removing

--- src/main.tsx
+++ src/main.tsx
@@ -20,11 +20,11 @@ i18next.init({
 });
 
 createRoot(document.getElementById("root")!).render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <StrictMode>
-      {/* <I18nextProvider i18n={i18next}> */}
+      <I18nextProvider i18n={i18next}>
         <App />
-      {/* </I18nextProvider> */}
+      </I18nextProvider>
     </StrictMode>
-  // </Provider>
+  </Provider>
 );
