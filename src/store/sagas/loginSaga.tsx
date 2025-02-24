2019aa5c97d141732ab90006b76f7bedcd1d4c92
RVPSA <akeshramasinghe@gmail.com>
2025-02-24 19:56:21 +0100

    this file handle the response from the backend, errors and exception, while contacting with the backend

--- src/store/sagas/loginSaga.tsx
+++ src/store/sagas/loginSaga.tsx
@@ -1,9 +1,38 @@
-import {takeLatest} from "redux-saga/effects"
+import { call, put, takeLatest } from "redux-saga/effects";
+import { USER_LOGIN, USER_LOGIN_FAIL, USER_LOGIN_SUCCESS } from "../actions";
+import { loginService } from "../../services/loginService";
+import { AxiosResponse } from "axios";
 
-function* userLogIn(){
+function* userLogIn(value: sagaInputType) {
+  const { data } = value;
 
+  try {
+    const response: AxiosResponse = yield call(loginService, data);
+
+    let receivedData: backendResponseGeneral = response.data;
+    console.log("Response in login saga,,receiveddata " +receivedData);
+    console.log("Response in login saga,,response " +response.data)
+
+    if (receivedData.statusCode == 200) {
+      //TODO Local Storage item
+      yield put({
+        type: USER_LOGIN_SUCCESS,
+        data: response.data.data,
+      });
+    } else {
+      yield put({
+        type: USER_LOGIN_FAIL,
+        data: response.data.message,
+      });
+    }
+  } catch (error) {
+    yield put({
+      type: USER_LOGIN_FAIL,
+      data: "User signin fail.",
+    });
+  }
 }
 
-export function* watchUserLogIn(){
-    yield takeLatest("",userLogIn)
+export function* watchUserLogIn() {
+  yield takeLatest(USER_LOGIN, userLogIn);
 }
