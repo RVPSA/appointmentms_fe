0f182fc25b7e21db7997053a34f4eeb6ab1ddaf3
RVPSA <akeshramasinghe@gmail.com>
2025-02-24 19:53:08 +0100

    developed the login reducer for that hndle the state of user login,loginfail and the loginsuccess

--- src/store/reducers/loginReducer.tsx
+++ src/store/reducers/loginReducer.tsx
@@ -1,3 +1,48 @@
-export const loginReducer = ()=>{
-    
-}
+import { USER_LOGIN, USER_LOGIN_FAIL, USER_LOGIN_SUCCESS } from "../actions";
+
+type stateType = {
+  isUserLogIn: boolean; //<> While forwarding the request
+  isUserLoginSuccess: boolean; //<> If login process is success
+  isUserLoginFail: boolean; //<> If login process is fail
+  userDetails: { userName?: string; password?: string }; //<> If login process success, response data
+  error: string; //<> If there is an error of request
+};
+
+const intialState: stateType = {
+  isUserLogIn: false,
+  isUserLoginSuccess: false,
+  isUserLoginFail: false,
+  userDetails: {},
+  error: "",
+};
+
+export const loginReducer = (
+  state: stateType = intialState,
+  action: actionType
+): stateType => {
+  switch (action.type) {
+    case USER_LOGIN:
+      console.log("Inside the user login reducer");
+      return {
+        ...state,
+        isUserLogIn: true,
+      };
+    case USER_LOGIN_SUCCESS:
+      let loginData = action.data;
+      return {
+        ...state,
+        isUserLogIn: false,
+        isUserLoginSuccess: true,
+        userDetails: loginData,
+      };
+    case USER_LOGIN_FAIL:
+      return {
+        ...state,
+        isUserLogIn: false,
+        isUserLoginSuccess: false,
+        isUserLoginFail: true,
+      };
+    default:
+      return state;
+  }
+};
