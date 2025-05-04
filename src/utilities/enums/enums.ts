4467f83a73a66ef36600070800b26b2c212acc7a
RVPSA <akeshramasinghe@gmail.com>
2025-05-04 16:13:35 +0530

    design a common component to display status

---
+++ src/utilities/enums/enums.ts
@@ -0,0 +1,11 @@
+export enum StatusEnum {
+  completed = 1,
+  inprogress = 2,
+  scheduled = 3,
+  cancelled = 4,
+  onGoing = 5,
+  managed = 6,
+  normal = 7,
+  attention = 8,
+  critical = 9,
+}
